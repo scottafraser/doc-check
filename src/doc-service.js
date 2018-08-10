d7281ea2f1c9f1a7f3daa5018dce7e17233cd9ff
Scott Fraser <scottafraser@yahoo.com>
2018-08-10 12:22:40 -0700

    card formating, clear button removes searches, begin builing second api call in services

--- src/doc-service.js
+++ src/doc-service.js
@@ -16,4 +16,20 @@ export class DocService {
         request.send();
       });
     }
+
+    getDocInfobyName(name) {
+        return new Promise(function(resolve, reject) {
+          let request = new XMLHttpRequest();       
+          let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${name}&location=or-portland&user_location=45.5122%2C%20-122.6587&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+          request.onload = function() {
+            if (this.status === 200) {
+              resolve(request.response);
+            } else {
+              reject(Error(request.statusText));
+            }
+          };
+          request.open("GET", url, true);
+          request.send();
+        });
+      }
   }
--- src/main.js
+++ src/main.js
@@ -4,9 +4,48 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { DocService } from'./doc-service.js';
 
+
+function patients(bool){
+  if (bool == true){
+    bool = "Accepting patients";
+    } else {
+    bool = "Not Accepting Patients";
+    }
+    return bool;
+}
+
+function template(body) {
+  for(let i = 0; i < body.data.length; i++) {
+    let name = body.data[i].profile.first_name + "" + body.data[i].profile.last_name;
+     let bool = patients(body.data[i].practices["0"].accepts_new_patients);
+     let number = body.data[i].practices["0"].phones["0"].number;
+     let address = body.data[i].practices["0"].visit_address.street;
+     let image = body.data[i].profile.image_url;
+     let website = body.data[i].practices["0"].website;       
+    
+      if (website == undefined){
+        website = "";
+      }
+
+      $('#info').append(
+        `<div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="${image}" alt="no pic">
+          <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <p class="card-text">${address}<br>${number}<br>${bool}<br>${website}</p>
+          </div>
+        </div>`);  
+    }
+}
+
 $(document).ready(function() {
   console.log("hey, you should buy Abe a beer sometime!");
-
+    //clear click
+    $('#clear-search').click(function(e) {
+      e.preventDefault();
+      $('.card').remove();
+    });
+    //symptom click
     $('#issue-submit').click(function(e) {
       e.preventDefault();
       let issue = $('#issue').val();
@@ -17,37 +56,27 @@ $(document).ready(function() {
 
       promise.then(function(response){
         let body = JSON.parse(response);
-        $('.showDoc').text(`${body.data["0"].profile.first_name} ${body.data["0"].profile.last_name} who can help with ${issue}`);
-        for(let i = 0; i < body.data.length; i++) {
-        let name = body.data[i].profile.first_name + "" + body.data[i].profile.last_name
-         let bool = body.data[i].practices["0"].accepts_new_patients
-         let number = body.data[i].practices["0"].phones["0"].number;
-         let address = body.data[i].practices["0"].visit_address.street
-         let image = body.data[i].profile.image_url
-         let website = body.data[i].practices["0"].website
-         
-         if (bool == true){
-          bool = "Accepting patients";
-          } else {
-          bool = "Not Accepting Patients";
-          }
 
-          if (website == undefined){
-            website = "";
-          }
+        template(body);
+      });
+    //name click
+    $('#name-submit').click(function(e) {
+      e.preventDefault();
+      let issue = $('#name').val();
+      $('#name').val("");
 
-          $('#info').append(
-            `<div class="info">
-              ${name}<br>
-              <img src='${image}'> <br>
-              ${number} <br> ${address} <br>
-              ${bool} <br> ${website} `);
+      let docService = new DocService();
+      let promise = docService.getDocInfobyIssue(issue);
 
-            
+      promise.then(function(response){
+        let body = JSON.parse(response);
 
-        }
+        template(body);
 
-        console.log(body);
+      });
+    
+
+        
       }, function(error) {
         $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
